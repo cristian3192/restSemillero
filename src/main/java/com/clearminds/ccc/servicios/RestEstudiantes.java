55399506ab86f8d92b17605b6f4ca5f6db5193de
Cristian <cris_t_15@hotmail.com>
2020-12-09 15:08:42 -0500

    Método actualizar en REstEstudiante

--- src/main/java/com/clearminds/ccc/servicios/RestEstudiantes.java
+++ src/main/java/com/clearminds/ccc/servicios/RestEstudiantes.java
@@ -2,11 +2,15 @@ package com.clearminds.ccc.servicios;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.clearminds.ccc.dtos.Estudiante;
 import com.clearminds.ccc.excepciones.BDDException;
+
+
 
 @Path("/estudiantes")
 public class RestEstudiantes {
@@ -22,4 +26,27 @@ public class RestEstudiantes {
 			e.printStackTrace();
 		}
 	}
+	
+	@Path("/actualizar")
+	@PUT
+	@Consumes({MediaType.APPLICATION_JSON})
+	@Produces({MediaType.APPLICATION_JSON})
+	public Estudiante actualizar(Estudiante estudiante){
+		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
+		try {
+			if (estudiante != null ) {
+				srvEstudiante.actualizarEstudiante(estudiante);
+				System.out.println("Estudiante Modificado: "+estudiante);	
+			}
+			System.out.println("LLega persona en null");
+
+					
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Estudiante();
+		}
+		
+		return estudiante;
+	}
+	
 }
